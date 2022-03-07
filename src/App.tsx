0c5dce97297a9b049cfa9c133275e6b38c343351
kvernev <kverenev@gmail.com>
2022-03-07 14:44:49 +0300

    add splash screens on ios and android

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Platform, UIManager } from 'react-native';
-import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+import {
+  gestureHandlerRootHOC,
+  //GestureHandlerRootView,
+} from 'react-native-gesture-handler';
 
 import { BottomTabsNavigator } from './navigation';
 import { AppProvider } from './App.provider';
@@ -12,13 +15,15 @@ if (Platform.OS === 'android') {
   }
 }
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return (
+    //<GestureHandlerRootView style={{ flex: 1 }}>
     <AppProvider>
       <NavigationContainer>
         <BottomTabsNavigator />
       </NavigationContainer>
     </AppProvider>
+    //</GestureHandlerRootView>
   );
 };
 
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, ImageBackground } from 'react-native';
 
 import { useAppContext } from '../hooks/useAppContext';
 import { MoodPicker } from '../components/MoodPicker';
+import SplashScreen from 'react-native-splash-screen';
 
 const backgroundImageUrl =
   'https://images.unsplash.com/photo-1474540412665-1cdae210ae6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1766&q=80';
 
 export const Home: React.FC = () => {
   const appContext = useAppContext();
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <ImageBackground
